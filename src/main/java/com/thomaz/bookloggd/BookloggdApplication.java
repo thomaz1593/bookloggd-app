56e7ba224bee365d9f5c0d952bfba3a5380ca8dc
Thomaz <thomaz1593@gmail.com>
2025-11-11 13:51:33 -0300

    Initial commit

---
+++ src/main/java/com/thomaz/bookloggd/BookloggdApplication.java
@@ -0,0 +1,13 @@
+package com.thomaz.bookloggd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookloggdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookloggdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomaz/bookloggd/BookloggdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomaz.bookloggd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookloggdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
